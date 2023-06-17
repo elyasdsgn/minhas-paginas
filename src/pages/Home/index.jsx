a091fab96e3b6e1aa8e31900f7b4eac0318bda23
Elyas Rodrigues <elyasrz11@gmail.com>
2023-06-17 13:02:30 -0300

    Inicio do projeto

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import Hero from "./partials/Header"
+
+function Home() {
+  return (
+    <>
+      <Hero/>
+      <main>
+      </main>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Home/partials/Header/index.jsx
@@ -0,0 +1,18 @@
+import "./hero.scss"
+
+function Hero() {
+  return (
+    <header className="hero">
+      <div className="container">
+        <div className="content d-flex">
+          <h1>Hello, World! Lorem ipsum dolor sit amet.</h1>
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum veritatis dolorem dolor corrupti veniam sunt ratione voluptatibus</h2>
+
+          <a href="#" className="button">QUERO ME INSCREVER</a>
+        </div>    
+      </div>
+    </header>
+  )
+}
+
+export default Hero
