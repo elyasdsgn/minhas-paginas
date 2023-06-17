6697e03a7a634ada11453d263f2e56164693ce8d
Elyas Rodrigues <elyasrz11@gmail.com>
2023-06-17 13:20:28 -0300

    inicio do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import Home from "./pages/Home"
+import Home from './pages/Home';
+import Bio from './pages/Bio';
+// import AOS from 'aos'
+// import "aos/dist/aos.css"
+
+import { createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom'
+// import { useEffect } from 'react';
+// import { useEffect } from 'react'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/bio",
+    element: <Bio/>
+  },
+  {
+    path: '*',
+    element: <Navigate to={'/'}/>
+  }
+])
 
 function App() {
+  // useEffect(() => {
+  //   AOS.init()
+
+  //   //Bloquear o arrasta das imagens
+  //   document.addEventListener('dragstart', (evento) => evento.preventDefault(), false)
+  //   document.addEventListener('drop', (evento) => evento.preventDefault(), false)
+
+  // }, [])
+  
 
   return (
     <>
-      <Home/>
+      <RouterProvider
+        router={router}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
